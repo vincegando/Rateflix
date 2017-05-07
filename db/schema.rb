d4f61cf7f8a3f5bd2be0974a3d67be6456d46c5c
vincegando <vgandolfo@umail.ucsb.edu>
2017-05-06 20:33:17 -0700

    most of site layout done

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170505022255) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "release_date"
